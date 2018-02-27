306449b48fc568328558ecba192119ed3e3d9933
Magnusducatus <magnusducatuslt@gmail.com>
2018-02-27 16:48:22 +0300

    + add static server form test + build css,js static + build js for html

---
+++ auth/auth.js
@@ -0,0 +1,46 @@
+
+
+swal({
+  title: 'Input username && password or private posting key',
+  html: '<p><div class="input-group mb-3">'+
+  			'<div class="input-group-prepend">'+
+    			'<span class="input-group-text" id="username">@</span>'+
+  			'</div>'+
+  			'<input id="input-user"type="text" class="form-control" placeholder="Username" aria-label="username" aria-describedby="username">'+
+		'</div>'+
+		'<p><div class="input-group mb-3">'+
+  			'<input id="input-pass" type="password" class="form-control" placeholder="password or private posting key" aria-label="password or posting key" aria-describedby="password">'+
+  			'<div class="input-group-prepend">'+
+   				'<span class="input-group-text" id="password"></span>'+
+  			'</div>'+
+		'</div>',
+  showCancelButton: true,
+  closeOnConfirm: false,
+  preConfirm: async ()=>{
+  	const {username, pass} = await getInputsVal();
+  	await checker(username, pass);
+
+  }
+})
+
+async function getInputsVal(){
+	let username = document.getElementById('input-user').value;
+  	let pass = document.getElementById('input-pass').value;
+  	return {username , pass};
+}
+
+async function checker(username, pass){
+	this.user = username;
+	this.pass = pass; // мастер-пароль
+	this.pass[0] == 5 ? console.log('приватный ключ',this.pass) :
+		golos.api.getAccounts([this.user], function(err, response) {
+			if ( ! err) {
+				// получение публичного ключа
+				var roles = ['posting']; // параметр необязательный, если не указаывать, то вернутся все ключи
+				var keys = golos.auth.getPrivateKeys(this.user, this.pass, roles);
+				// проверка публичного ключа аккаунта и с полученым публичным ключом
+				if (response[0].posting.key_auths[0][0] == keys.postingPubkey) console.log('правильный логин и мастер-пароль!');
+				else console.log('не правильный логин и\или мастер-пароль!');
+			}
+		});
+}
