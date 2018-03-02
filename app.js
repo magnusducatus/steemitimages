59b687cd3312b1a0b00a68da00dcb9ed4956c660
Magnusducatus <magnusducatuslt@gmail.com>
2018-03-02 13:11:40 +0300

    -iniZone + min sweet

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const host = 'http://91.201.41.253:7777/ipfs/';
  function _arrayBufferToBase64(buffer) {
      var binary = '';
      var bytes = new Uint8Array(buffer);
+     console.log(typeof bytes);
      var len = bytes.byteLength;
      for (var i = 0; i < len; i++) {
          binary += String.fromCharCode(bytes[i]);
@@ -42,7 +43,10 @@ const host = 'http://91.201.41.253:7777/ipfs/';
  function _arrayBuffer(buffer) {
      var binary = '';
      var bytes = new Uint8Array(buffer);
-     var blob = new Blob( [ bytes ], { type: "image/jpeg" } );
+     var len = bytes.byteLength;
+     for (var i = 0; i < len; i++) {
+         binary += String.fromCharCode(bytes[i]);
+     }
      return binary;
  }
  //Функция самой отправки данных
@@ -53,7 +57,6 @@ const host = 'http://91.201.41.253:7777/ipfs/';
          path: data.name,
          content: data.body
      }];
-
      /*ipfs.files.add(new node.types.Buffer(data.body), function(err, file) {*/
      ipfs.files.add(files, function(err, file) {
          if (err) console.log(err);
@@ -145,3 +148,71 @@ const host = 'http://91.201.41.253:7777/ipfs/';
  }
  const upload = document.getElementById('uploadBtn');
  upload.addEventListener("click", iter, false);
+
+ //init Dropzone
+Dropzone.options.dropzone = {
+ //accept file mime-type
+ acceptedFiles: 'image/jpeg, image/jpg, image/png',
+ dictDefaultMessage: "Drag&Drop files here or click to select files",
+ autoProcessQueue: false,
+ init: function() {
+     this.on("addedfile", function(file) {
+         //second check for mime-type
+         if (file.type != 'image/jpeg' || file.type != 'image/jpg' || file.type != 'image/png') {
+
+         } else this.removeFile(file);
+         //entity for send to IPFS
+
+         //console.log(arr1);
+         let fileList = file;
+         const reader = new FileReader();
+         reader.onload = function(data) {
+             const obj = {
+                 name: '',
+                 body: '',
+                 hash: '',
+             };
+             //!!!!!This is for version without buffer
+             obj.body = ipfs.Buffer(data.target.result);
+             //obj.body = data.target.result;
+             obj.name = fileList.name;
+             arr1.push(obj);
+             let uploadBtn = document.getElementById('uploadBtn');
+             uploadBtn.style.display = "block";
+         };
+         reader.readAsArrayBuffer(fileList);
+         // Create the remove button
+         var removeButton = Dropzone.createElement('<button class="btn btn-warning">Remove file</button>');
+         // Capture the Dropzone instance as closure.
+         var _this = this;
+         //remove all files
+         document.getElementById("uploadBtn").addEventListener("click", function() {
+             _this.removeAllFiles();
+             arr1 = [];
+             let uploadBtn = document.getElementById('uploadBtn');
+             uploadBtn.style.display = "none";
+         });
+         // Listen to the click event
+         removeButton.addEventListener("click", function(e) {
+             // Make sure the button click doesn't submit the form:
+             e.preventDefault();
+             e.stopPropagation();
+
+             // Remove the file preview.
+             _this.removeFile(file);
+             let uploadBtn = document.getElementById('uploadBtn');
+
+             for (let i = 0; i < arr1.length; i++) {
+                 file.name == arr1[i].name ? arr1.splice(i, 1) : '';
+             }
+             arr1.length > 0 ? uploadBtn.style.display = "block" : uploadBtn.style.display = "none";
+         });
+
+         // Add the button to the file preview element.
+         file.previewElement.appendChild(removeButton);
+         file.previewElement.setAttribute('class', 'dz-preview dz-processing dz-success dz-complete dz-image-preview elementIpfs');
+         file.previewElement.setAttribute('align', 'center');
+     });
+ }
+
+};
