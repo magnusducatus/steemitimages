4c40db2b02712b4356c837b9d766f40c1f6cd125
Magnusducatus <magnusducatuslt@gmail.com>
2018-03-29 17:36:10 +0300

    + integration_example connectionCustom + update golos

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ swal.setDefaults({
 });
 
 
-golos.config.set('websocket', 'wss://ws.testnet3.golos.io');
+golos.config.set('websocket', 'wss://ws.testnet.golos.io');
 golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 
 
