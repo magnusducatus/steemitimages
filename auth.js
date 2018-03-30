8598e1aed71326e4cce0adfcc10499d9cbe7e326
Magnusducatus <magnusducatuslt@gmail.com>
2018-03-30 11:28:06 +0300

    + translate auth

--- auth.js
+++ auth.js
@@ -3,8 +3,8 @@
     localStorage && localStorage.username ? window.username = localStorage.username : window.username = '';
     async function auth(cb = function(){}){
         const {value} = await swal({
-        title:  `<h3>To continue, you need to login!</h3>`,
-        html:`<p><h5>Please enter your login and master password</h5></p>
+        title:  document.getElementById('auth-title').innerHTML,
+        html:`${document.getElementById('auth-html-logorpass').innerHTML}
                 <p><div class="input-group mb-3">
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="username" required>@</span>
@@ -14,8 +14,8 @@
                 <p><div>
                         <input id="input-pass" type="password" class="form-control" placeholder="Master password" aria-label="Password" aria-describedby="Password" required>
                    </div>
-                <p><hr class="bg-secondary">OR<hr class="bg-secondary">
-                <p><h5>Please enter only your private posting key</h5>
+                <p><hr class="bg-secondary">${document.getElementById('auth-html-or').innerHTML}<hr class="bg-secondary">
+                <p><h5>${document.getElementById('auth-html-postKey').innerHTML}</h5>
                 <p><div>
                     <input id="input-private" type="password" class="form-control" placeholder="Private posting key" aria-label="Private posting key" aria-describedby="Private posting key" required>
                 </div>
@@ -23,7 +23,7 @@
                 <div class="form-check">
                     <label class="form-check-label">
                       <input id="logged" class="form-check-input" type="checkbox" value="">
-                      Keep me logged
+                      ${document.getElementById('auth-html-keepLog').innerHTML}
                     </label>
                 </div>`,
         showCancelButton: true,
@@ -31,7 +31,7 @@
         showCloseButton: true,
         footer:`<div class="d-flex flex-column  align-items-center">
                     <div>
-                        OR<hr class="bg-secondary">
+                        ${document.getElementById('auth-html-or').innerHTML}<hr class="bg-secondary">
                     </div>
                     <a class="swal2-actions" target="_blank" href="https://golos.io/create_account">
                         <button type="button" class="swal2-cancel swal2-styled" aria-label="" style="display: inline-block; background-color: #297dce;"><span class="icon-info"></span> Sign Up</button>
