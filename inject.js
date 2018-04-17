88ff4dc2400cfa42302ba36007c94650e479ca17
Magnusducatus <magnusducatuslt@gmail.com>
2018-04-17 14:17:57 +0300

    +

--- inject.js
+++ inject.js
@@ -1,9 +1,9 @@
 
 	let ipfsApi = document.createElement('script');
 	ipfsApi.src = 'https://unpkg.com/ipfs-api@9.0.0/dist/index.min.js';
-	(document.head||document.documentElement).appendChild(ipfsApi);
+	(document.head || document.documentElement).appendChild(ipfsApi);
 	let golosImagesApi = document.createElement('script');
 	golosImagesApi.src = 'https://golosimages.com/api.js';
 	//golosImagesApi.src = 'api.js';
-	(document.head||document.documentElement).appendChild(golosImagesApi);
+	(document.head || document.documentElement).appendChild(golosImagesApi);
 
